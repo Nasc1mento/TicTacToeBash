e09d06793ee636d787dfbfc7a2532be55cc23b3c
Nasc1mento <reisadryan16@gmail.com>
2022-10-01 11:35:57 -0300

    finally i found the overscore

--- tic_tac_toe.sh
+++ tic_tac_toe.sh
@@ -2,23 +2,44 @@
 
 
 declare -A values
-
 values=([1]=1 [2]=2 [3]=3 [4]=4 [5]=5 [6]=6 [7]=7 [8]=8 [9]=9)
 
-echo -e "\n"
-echo -e "\t     |     |"
-echo -e "\t  "${values[1]}"  |  "${values[2]}"  |  "${values[3]}""
-echo -e "\t_____|_____|_____"
-echo -e "\t     |     |"
-echo -e "\t  "${values[4]}"  |  "${values[5]}"  |  "${values[6]}""
-echo -e "\t_____|_____|_____"
-echo -e "\t     |     |"
-echo -e "\t  "${values[7]}"  |  "${values[8]}"  |  "${values[9]}""
-echo -e "\t     |     |"
-echo -e "\n"
 
 
 
+table(){
+    echo -e "\n"
+    echo -e "\t|‾‾‾‾‾|‾‾‾‾‾|‾‾‾‾‾|"
+    echo -e "\t|  ${values[1]}  |  ${values[2]}  |  ${values[3]}  |"
+    echo -e "\t|_____|_____|_____|"
+    echo -e "\t|     |     |     |"
+    echo -e "\t|  ${values[4]}  |  ${values[5]}  |  ${values[6]}  |"
+    echo -e "\t|_____|_____|_____|"
+    echo -e "\t|     |     |     |"
+    echo -e "\t|  ${values[7]}  |  ${values[8]}  |  ${values[9]}  |"
+    echo -e "\t|_____|_____|_____|"
+    echo -e "\n"
+}
+
+table
+check_board(){
+    echo "s"
+}
+
+loop(){
+    echo ""
+}
+
+
+change(){
+    echo ""
+}
+
+
+
+main(){
+    echo ""
+}
 
 
 
