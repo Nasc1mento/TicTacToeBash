413675913cbdbad69e9d0d1549155f2f948b94d5
Nasc1mento <reisadryan16@gmail.com>
2022-10-04 22:25:25 -0300

    improve persistence code

--- lib/persistence.sh
+++ lib/persistence.sh
@@ -8,7 +8,8 @@ persistence(){
 
 show_persistence(){
     _PRINT "history.txt"
-    [ -f $File ] && sed 's/,,/, ,/g;s/,,/, ,/g' $File | column -s, -t || echo "Empty!"
+    # File exists and it's empty
+    [ -f $File ] && [ -s $File ] && sed 's/,,/, ,/g;s/,,/, ,/g' $File | column -s, -t || echo "Empty!"
 }
     
 
